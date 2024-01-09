f2f231ab016088969df6862d0386dd47ac287226
Gaurav Sah <145892366+GauravSah21@users.noreply.github.com>
2024-01-09 15:07:19 +0545

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const endDate = "01 January 2025 00:00:00"; // Updated end date
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+function padZero(number) {
+    return number < 10 ? "0" + number : number;
+}
+
+function clock() {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now) / 1000;
+
+    if (diff < 0) return;
+
+    // convert into days;
+    inputs[0].value = padZero(Math.floor(diff / 3600 / 24));
+    inputs[1].value = padZero(Math.floor(diff / 3600) % 24);
+    inputs[2].value = padZero(Math.floor(diff / 60) % 60);
+    inputs[3].value = padZero(Math.floor(diff) % 60);
+}
+
+// Initial call
+clock();
+
+/**
+ *  1 day = 24 hrs
+ *  1 hr = 60 mins
+ *  60 min = 3600 sec
+ */
+
+setInterval(() => {
+    clock();
+}, 1000);
